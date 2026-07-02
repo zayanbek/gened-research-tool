0cb6ed599d6e602244419b8d50329f8b660a6147
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-02 17:08:35 +0530

    scaffold spring project

---
+++ backend/src/main/java/com/zayan/gened_researcher_tool/GenedResearcherToolApplication.java
@@ -0,0 +1,13 @@
+package com.zayan.gened_researcher_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenedResearcherToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenedResearcherToolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zayan/gened_researcher_tool/GenedResearcherToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zayan.gened_researcher_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenedResearcherToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
