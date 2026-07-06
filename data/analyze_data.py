756062d61700b09e9df41e2288526f41bc68631f
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-06 18:05:12 +0530

    analyze data on courses gen ed csv

--- data/analyze_data.py
+++ data/analyze_data.py
@@ -3,11 +3,30 @@
 
 import pandas as pd
 
-file_name = "uiuc-gpa-dataset.csv"
+file_name = "data/uiuc-gened-course-information.csv"
 
 df = pd.read_csv(file_name)
 
 for column in df.columns:
     max_length = df[f"{column}"].astype(str).str.len().max()
     
-    print(f"\"{column}\" \t	 {max_length}")
+    print(f"\"{column}\"\t\t{max_length}")
+    
+print()
+
+"""
+CREATE TABLE load_courses_gen_ed_csv (
+    Year INTEGER,
+    Term VARCHAR(10),
+    TermYear VARCHAR(10),
+    Course VARCHAR(10),
+    Course Title VARCHAR(100),
+    ACP VARCHAR(5),
+    CS VARCHAR(5),
+    HUM VARCHAR(5),
+    NAT VARCHAR(5),
+    QR VARCHAR(5),
+    SBS VARCHAR(5),
+);  
+  
+"""
