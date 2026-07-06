d1f4c67c3a65505cdb0a7b3253f45a1546203454
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-06 20:51:30 +0530

    create

---
+++ frontend/src/components/gpaSlider/GpaSlider.tsx
@@ -0,0 +1,49 @@
+import { Range, getTrackBackground } from "react-range";
+
+type GpaSliderProps = {
+  values: number[];
+  setValues: (values: number[]) => void;
+};
+
+export default function GpaSlider({
+  values,
+  setValues,
+}: GpaSliderProps) {
+  return (
+    <Range
+      step={0.1}
+      min={0}
+      max={4}
+      values={values}
+      onChange={setValues}
+      renderTrack={({ props, children }) => (
+  <div
+    {...props}
+    className="gpa-slider__track"
+    style={{
+      ...props.style,
+      background: getTrackBackground({
+        values,
+        colors: [
+          "var(--secondary)", // left of first thumb
+          "var(--primary)",   // between thumbs
+          "var(--secondary)", // right of second thumb
+        ],
+        min: 0,
+        max: 4,
+      }),
+    }}
+  >
+    {children}
+  </div>
+)}
+      renderThumb={({ props }) => (
+        <div
+          {...props}
+          className="gpa-slider__thumb"
+        />
+      )}
+      
+    />
+  );
+}
