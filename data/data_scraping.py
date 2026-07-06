058a7b45375aec0cb2d4fa0432de1840d4afa07b
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-06 18:05:32 +0530

    refactor data scraping logic and database connection handling

--- data/data_scraping.py
+++ data/data_scraping.py
@@ -2,23 +2,43 @@ import os
 import psycopg
 from dotenv import load_dotenv
 
+import requests
+from bs4 import BeautifulSoup
+    
 load_dotenv()
-def get_connection():
-    return psycopg.connect(
+
+# establish database connection
+ 
+db_connection = psycopg.connect(
         host="localhost",
         port=5432,
         dbname="uiuc-gened-gpa-database",
-        user="postgres",
+        user=os.getenv("DB_USERNAME"),
         password=os.getenv("DB_PASSWORD")
     )
-    
-conn = get_connection()
-cur = conn.cursor()
 
-cur.execute("SELECT * FROM distinct_courses LIMIT 10;")
+cur = db_connection.cursor()
 
-for row in cur.fetchall():
-    print(row)
+# Start data scraping
 
+url = "https://courses.illinois.edu/gened/2026/fall/ACP"
+
+response = requests.get(url)
+
+print(response.status_code)
+print(response.url)
+print(response.text[:1000])
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+tbody = soup.find("table", id="gened-req-table").find("tbody")
+
+tbody_rows = tbody.find_all("tr")
+
+for row in tbody_rows:
+    cells = row.find_all("td")
+    print(cells)
+
+# close database connection
 cur.close()
-conn.close()
+db_connection.close()
