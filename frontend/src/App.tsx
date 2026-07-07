103dceb79086c083b6c79bf16ed58b8382e98e96
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-07 17:55:40 +0530

    show SearchResults page instead of Home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import Home from "./pages/home/Home";
+import SearchResults from "./pages/searchResults/SearchResults";
 
 function App() {
-  return (
-    <Home />
-  );
+  // return <Home />;
+  return <SearchResults />;
 }
 
 export default App;
