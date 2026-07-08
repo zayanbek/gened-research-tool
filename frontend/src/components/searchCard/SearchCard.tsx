d2708844544d3e037f7315e87fd53a8e972a1319
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-08 13:36:06 +0530

    implement navigation in SearchCard component for results page

--- frontend/src/components/searchCard/SearchCard.tsx
+++ frontend/src/components/searchCard/SearchCard.tsx
@@ -6,7 +6,11 @@ import CourseNameFilter from "../searchFilters/CourseNameFilter";
 import GpaRangeFilter from "../searchFilters/GpaRangeFilter";
 import OfferedThisTermFilter from "../searchFilters/OfferedThisTermFilter";
 
+import { useNavigate } from "react-router-dom";
+
 export default function SearchCard() {
+  const navigate = useNavigate();
+
   return (
     <aside className="search-card">
       <h2 className="search-card__title">Search Filters</h2>
@@ -23,7 +27,9 @@ export default function SearchCard() {
 
       <div className="search-card__buttons">
         <button className="secondary">Reset</button>
-        <button className="primary">Search</button>
+        <button className="primary" onClick={() => navigate("/results")}>
+          Search
+        </button>
       </div>
     </aside>
   );
