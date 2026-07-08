65a80e9ec7cfdd754d9cd9e9972b87191e0392e3
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-08 15:38:41 +0530

    create GenedCategoryFilter

---
+++ frontend/src/components/searchFilters/GenedCategoryFilter.tsx
@@ -0,0 +1,7 @@
+export default function GenedCategoryFilter() {
+  return (
+    <div className="search-card__section">
+      <label htmlFor="courseNumber">Gen ed category filter</label>
+    </div>
+  );
+}
