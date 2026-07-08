29213cc9ab34cb598365c5214f14595259cb9947
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-08 13:35:19 +0530

    implement Sidebar component with search filters and buttons

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,26 @@
 import "./Sidebar.css";
 
-export default function Header() {
-  return <h1>Hello World</h1>;
+import SubjectFilter from "../searchFilters/SubjectFilter";
+import CourseNumberFilter from "../searchFilters/CourseNumberFilter";
+import CourseNameFilter from "../searchFilters/CourseNameFilter";
+import GpaRangeFilter from "../searchFilters/GpaRangeFilter";
+import OfferedThisTermFilter from "../searchFilters/OfferedThisTermFilter";
+
+export default function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h2 className="sidebar__title">Search Filters</h2>
+
+      <SubjectFilter />
+      <CourseNumberFilter />
+      <CourseNameFilter />
+      <GpaRangeFilter />
+      <OfferedThisTermFilter />
+
+      <div className="sidebar__buttons">
+        <button className="secondary">Reset</button>
+        <button className="primary">Search</button>
+      </div>
+    </aside>
+  );
 }
