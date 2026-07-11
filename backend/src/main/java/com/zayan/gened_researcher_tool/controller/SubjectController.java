5e89f2b9d181cb4e2eb5495ba75606b8c41207ad
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-11 13:22:37 +0530

    Delegate subject search to service

--- backend/src/main/java/com/zayan/gened_researcher_tool/controller/SubjectController.java
+++ backend/src/main/java/com/zayan/gened_researcher_tool/controller/SubjectController.java
@@ -2,7 +2,6 @@ package com.zayan.gened_researcher_tool.controller;
 
 import com.zayan.gened_researcher_tool.entity.Subject;
 import com.zayan.gened_researcher_tool.service.SubjectService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,12 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/subjects")
+@RequestMapping("/subjects")
 public class SubjectController {
 
     private final SubjectService subjectService;
 
-    @Autowired
     public SubjectController(SubjectService subjectService) {
         this.subjectService = subjectService;
     }
@@ -24,16 +22,8 @@ public class SubjectController {
     @GetMapping
     public List<Subject> getSubjects(
             @RequestParam(required = false) String code,
-            @RequestParam(required = false) String name
-    ) {
-        if(code != null && name != null) {
-            return subjectService.getSubjectByCodeAndName(code, name);
-        } else if (code != null) {
-            return subjectService.getSubjectByCode(code);
-        } else if (name != null) {
-            return subjectService.getSubjectByName(name);
-        } else {
-            return subjectService.getSubjects();
-        }
+            @RequestParam(required = false) String name) {
+
+        return subjectService.searchSubjects(code, name);
     }
 }
