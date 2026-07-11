dc257617cc7eb7ea64375d34c51c46f5b3f799e7
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-11 17:29:32 +0530

    Add fields to CourseDescriptionDto for description and GPA history

--- backend/src/main/java/com/zayan/gened_researcher_tool/dto/CourseDescriptionDto.java
+++ backend/src/main/java/com/zayan/gened_researcher_tool/dto/CourseDescriptionDto.java
@@ -1,4 +1,10 @@
 package com.zayan.gened_researcher_tool.dto;
 
+import java.util.List;
+
 public class CourseDescriptionDto {
+
+    private String description;
+    private List<Double> gpaHistory;
+
 }
