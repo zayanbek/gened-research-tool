9713b1cdcdb2c38feff089d988e3e00536543bdd
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-11 13:27:37 +0530

    Add InstructorRepository interface

---
+++ backend/src/main/java/com/zayan/gened_researcher_tool/repository/InstructorRepository.java
@@ -0,0 +1,12 @@
+package com.zayan.gened_researcher_tool.repository;
+
+import com.zayan.gened_researcher_tool.entity.Instructor;
+import com.zayan.gened_researcher_tool.entity.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InstructorRepository extends JpaRepository<Instructor, Integer>,
+        JpaSpecificationExecutor<Instructor> {
+}
