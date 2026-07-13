6372919b4dbc105e15934af060afb8b56b2dde72
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-13 22:28:58 +0530

    Wrap App component with SubjectProvider for context management

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
 import "./theme.css";
+import { SubjectProvider } from "./api/SubjectContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <SubjectProvider>
+      <App />
+    </SubjectProvider>
   </StrictMode>,
-)
+);
