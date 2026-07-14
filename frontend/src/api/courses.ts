49d3c7323d69b054b414092212d1df70bad1f247
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-15 01:02:39 +0530

    add wasOffered filter to frontend api

--- frontend/src/api/courses.ts
+++ frontend/src/api/courses.ts
@@ -4,15 +4,23 @@ export async function searchCourses(filters: CourseSearchRequest) {
   const params = new URLSearchParams();
 
   if (filters.subject) params.append("subject", filters.subject);
+
   if (filters.number !== undefined)
     params.append("number", filters.number.toString());
+
   if (filters.level !== undefined)
     params.append("level", filters.level.toString());
+
   if (filters.minGpa !== undefined)
     params.append("minGpa", filters.minGpa.toString());
+
   if (filters.maxGpa !== undefined)
     params.append("maxGpa", filters.maxGpa.toString());
+
   if (filters.title) params.append("title", filters.title);
+
+  if (filters.offered !== undefined)
+    params.append("offered", filters.offered.toString());
 
   filters.genEdCodes?.forEach((code) => params.append("genEdCodes", code));
 
