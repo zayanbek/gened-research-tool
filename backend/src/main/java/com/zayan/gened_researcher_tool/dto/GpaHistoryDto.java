5b9dc3a65904c97d289eda6a65026230d06e7272
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-16 23:01:19 +0530

    implement class

--- backend/src/main/java/com/zayan/gened_researcher_tool/dto/GpaHistoryDto.java
+++ backend/src/main/java/com/zayan/gened_researcher_tool/dto/GpaHistoryDto.java
@@ -1,4 +1,38 @@
 package com.zayan.gened_researcher_tool.dto;
 
 public class GpaHistoryDto {
+
+    private int year;
+    private String term;
+    private double gpa;
+
+    public GpaHistoryDto(int year, String term, double gpa) {
+        this.year = year;
+        this.term = term;
+        this.gpa = gpa;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
 }
