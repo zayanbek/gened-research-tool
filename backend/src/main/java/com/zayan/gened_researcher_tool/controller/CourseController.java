9ae7a0a2b1fb8f54af31b4187e70b20d6120c27e
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-22 22:00:11 +0530

    update getCourses method to use CourseSearchRequestDto and include sorting parameters

--- backend/src/main/java/com/zayan/gened_researcher_tool/controller/CourseController.java
+++ backend/src/main/java/com/zayan/gened_researcher_tool/controller/CourseController.java
@@ -1,7 +1,8 @@
 package com.zayan.gened_researcher_tool.controller;
 
 import com.zayan.gened_researcher_tool.dto.CourseDescriptionDto;
-import com.zayan.gened_researcher_tool.dto.CourseSearchDto;
+import com.zayan.gened_researcher_tool.dto.CourseSearchRequestDto;
+import com.zayan.gened_researcher_tool.dto.CourseSearchResultDto;
 import com.zayan.gened_researcher_tool.service.CourseService;
 
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,7 @@ public class CourseController {
     }
 
     @GetMapping
-    public List<CourseSearchDto> getCourses(
+    public List<CourseSearchResultDto> getCourses(
             @RequestParam(required = false) String subject,
             @RequestParam(required = false) Integer number,
             @RequestParam(required = false) Integer level,
@@ -27,9 +28,23 @@ public class CourseController {
             @RequestParam(required = false) Double maxGpa,
             @RequestParam(required = false) String title,
             @RequestParam(required = false) List<String> genEdCodes,
-            @RequestParam(required = false) Boolean wasOffered
+            @RequestParam(required = false) Boolean wasOffered,
+            @RequestParam(defaultValue = "courseTitle") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDirection
     ) {
-        return courseService.searchCourses(subject, number, level, minGpa, maxGpa, title, genEdCodes, wasOffered);
+        return courseService.searchCourses(new CourseSearchRequestDto(
+                    subject,
+                    number,
+                    level,
+                    minGpa,
+                    maxGpa,
+                    title,
+                    genEdCodes,
+                    wasOffered,
+                    sortBy,
+                    sortDirection
+                )
+        );
     }
 
     @GetMapping("/{id}")
