3308a8930bb3dbbbd55d9450d8d3cffe99cd7e30
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-22 19:27:15 +0530

    color code gen-ed tags

--- frontend/src/components/resultsCard/ResultsCard.tsx
+++ frontend/src/components/resultsCard/ResultsCard.tsx
@@ -7,6 +7,31 @@ type ResultsCardProps = {
   onClick: () => void;
 };
 
+const genEdClass: Record<string, string> = {
+  ACP: "tag--acp",
+
+  HUM: "tag--hum",
+  HP: "tag--hum",
+  LA: "tag--hum",
+
+  SBS: "tag--sbs",
+  BSC: "tag--sbs",
+  SS: "tag--sbs",
+
+  NAT: "tag--nat",
+  LS: "tag--nat",
+  PS: "tag--nat",
+
+  CS: "tag--cs",
+  NW: "tag--cs",
+  US: "tag--cs",
+  WCC: "tag--cs",
+
+  QR: "tag--qr",
+  QR1: "tag--qr",
+  QR2: "tag--qr",
+};
+
 export default function ResultsCard({ course, onClick }: ResultsCardProps) {
   return (
     <aside className="results-card" onClick={onClick}>
@@ -18,7 +43,10 @@ export default function ResultsCard({ course, onClick }: ResultsCardProps) {
 
       <div className="results-card__geneds">
         {course.genEdCodes.map((code) => (
-          <span key={code} className="results-card__tag">
+          <span
+            key={code}
+            className={`results-card__tag ${genEdClass[code] ?? ""}`}
+          >
             {code}
           </span>
         ))}
