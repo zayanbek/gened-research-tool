380968de9ad1edbfc37051f5c03818fd4ae6440c
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-24 21:54:42 +0530

    add timeExcellent and timesOutstanding columns, along with the corresponding getters and setters; add two more parameters to constructor

--- backend/src/main/java/com/zayan/gened_researcher_tool/entity/Instructor.java
+++ backend/src/main/java/com/zayan/gened_researcher_tool/entity/Instructor.java
@@ -13,14 +13,24 @@ public class Instructor {
 
     private String name;
 
+    @Column(name="times_excellent")
+    private Integer timesExcellent;
+
+    @Column(name="times_outstanding")
+    private Integer timesOutstanding;
+
     @OneToMany(mappedBy = "instructor")
     private List<CourseInformation> courses;
 
     public Instructor() {
     }
 
-    public Instructor(Integer id) {
+    public Instructor(Integer id, String name, Integer timesExcellent, Integer timesOutstanding, List<CourseInformation> courses) {
         this.id = id;
+        this.name = name;
+        this.timesExcellent = timesExcellent;
+        this.timesOutstanding = timesOutstanding;
+        this.courses = courses;
     }
 
     public Integer getId() {
@@ -45,5 +55,21 @@ public class Instructor {
 
     public void setCourses(List<CourseInformation> courses) {
         this.courses = courses;
+    }
+
+    public Integer getTimesExcellent() {
+        return timesExcellent;
+    }
+
+    public void setTimesExcellent(Integer timesExcellent) {
+        this.timesExcellent = timesExcellent;
+    }
+
+    public Integer getTimesOutstanding() {
+        return timesOutstanding;
+    }
+
+    public void setTimesOutstanding(Integer timesOutstanding) {
+        this.timesOutstanding = timesOutstanding;
     }
 }
