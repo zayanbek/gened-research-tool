2768ecfcc4451103f2441e15e4681c0aa0563d93
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-26 23:58:41 +0530

    add rate my professor dto

---
+++ frontend/src/types/RateMyProfessorDto.ts
@@ -0,0 +1,9 @@
+export interface RateMyProfessorDto {
+  formattedName: string;
+  avgRating: number;
+  avgDifficulty: number;
+  numRatings: number;
+  wouldTakeAgainPercent: number;
+  department: string;
+  link: string;
+}
--- frontend/src/types/TeacherInformationDto.ts
+++ frontend/src/types/TeacherInformationDto.ts
@@ -1,5 +1,8 @@
+import type { RateMyProfessorDto } from "./RateMyProfessorDto";
+
 export interface TeacherInformationDto {
   name: string;
   excellent: number;
   outstanding: number;
+  rateMyProfessor: RateMyProfessorDto;
 }
