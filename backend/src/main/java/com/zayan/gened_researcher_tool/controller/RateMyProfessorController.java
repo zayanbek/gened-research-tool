d5ec52d3828d184f4cfa0a1eed289b7360cffaf2
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-11 18:12:04 -0500

    create api endpoint for rate my professor service

---
+++ backend/src/main/java/com/zayan/gened_researcher_tool/controller/RateMyProfessorController.java
@@ -0,0 +1,26 @@
+package com.zayan.gened_researcher_tool.controller;
+
+import com.zayan.gened_researcher_tool.dto.RateMyProfessorDto;
+import com.zayan.gened_researcher_tool.service.CourseService;
+import com.zayan.gened_researcher_tool.service.RateMyProfessorService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rate-my-professor/professors")
+public class RateMyProfessorController {
+
+     private final RateMyProfessorService rateMyProfessorService;
+
+     public RateMyProfessorController(RateMyProfessorService rateMyProfessorService) {
+          this.rateMyProfessorService = rateMyProfessorService;
+     }
+
+     @GetMapping("/{instructorName}")
+     public RateMyProfessorDto getTeacherInformation(@PathVariable String instructorName) {
+          return rateMyProfessorService.getProfessor(instructorName);
+     }
+
+}
