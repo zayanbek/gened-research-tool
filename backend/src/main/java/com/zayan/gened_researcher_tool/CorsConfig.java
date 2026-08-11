67301f2bbde733ad0d90f6387a3d124b3f946ce8
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-11 13:50:52 -0500

    remove 5173 port from cors configuration

--- backend/src/main/java/com/zayan/gened_researcher_tool/CorsConfig.java
+++ backend/src/main/java/com/zayan/gened_researcher_tool/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:4173")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
