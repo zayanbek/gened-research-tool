fa5bbc133eb73de23cc06f5092c91f3855b8f3e9
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-12 17:20:15 -0500

    remove unused import in rmp controller

--- backend/src/main/java/com/zayan/gened_researcher_tool/controller/RateMyProfessorController.java
+++ backend/src/main/java/com/zayan/gened_researcher_tool/controller/RateMyProfessorController.java
@@ -1,7 +1,6 @@
 package com.zayan.gened_researcher_tool.controller;
 
 import com.zayan.gened_researcher_tool.dto.RateMyProfessorDto;
-import com.zayan.gened_researcher_tool.service.CourseService;
 import com.zayan.gened_researcher_tool.service.RateMyProfessorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
